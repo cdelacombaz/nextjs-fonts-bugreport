3b1c68632970aa43603dc342fdd408e2fcb3b48b
cdelacombaz <cdelacombaz@bluewin.ch>
2022-02-15 18:19:14 +0100

    Initial commit

---
+++ project/pages/_app.jsx
@@ -0,0 +1,17 @@
+// import '../style/global.css'
+import {ThemeProvider} from 'styled-components'
+
+import {GlobalStyle, defaultTheme} from '../style'
+
+const MyApp = ({Component, pageProps}) => {
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={defaultTheme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
+}
+
+export default MyApp
